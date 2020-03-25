82f5a219b5c576294921843453a5a4c82269052b
face9363 <4181hiro@gmail.com>
2020-03-26 00:18:49 +0900

    controller separated

--- src/main.rs
+++ src/main.rs
@@ -4,56 +4,14 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket_contrib::json::Json;
-use crate::models::{Community, NoIdCommunity};
-
 mod controllers;
 mod models;
 mod mapper;
 mod services;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/communities?<search>")]
-fn community_search(search: Option<String>) -> Json<Vec<Community>> {
-    return match search {
-        Some(s) => Json(services::community::search_community(&s)),
-        None => panic!()
-    }
-}
-
-#[get("/communities/<_id>")]
-fn community_detail(_id: Option<i32>) -> Json<Community> {
-    return match _id {
-        Some(i) => Json(services::community::find_community(i)),
-        None => panic!()
-    }
-}
-
-#[post("/communities", data = "<community>")]
-fn community_create(community: Json<NoIdCommunity>) -> Json<Community> {
-    Json(services::community::create_community(NoIdCommunity {
-        name: community.name.to_string(),
-        description: community.description.to_string(),
-        public: community.public
-    }))
-}
-
-#[put("/communities/<_id>", data = "<community>")]
-fn community_update(_id: i32, community: Json<NoIdCommunity>) -> Json<Community> {
-    Json(services::community::update_community(Community{
-        id: _id,
-        name: community.name.to_string(),
-        description: community.description.to_string(),
-        public: community.public
-    }))
-}
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, community_search, community_detail, community_create, community_update])  // ここにルーティングをセットする
+        .mount("/", controllers::community::router())
         .launch();
 }
