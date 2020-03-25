28c0ab32341afd304a14b964cadb58a5d79033cf
face9363 <4181hiro@gmail.com>
2020-03-26 02:48:52 +0900

    refactor using map

--- src/mapper/community.rs
+++ src/mapper/community.rs
@@ -18,31 +18,35 @@ pub fn find(_id: i32) -> Result<Community, String> {
         })
 }
 
-pub fn create(community: &NoIdCommunity) -> Result<Community, &'static str> {
-    let mut new_community = Err("no community");
-    for row in get_client().query(
+pub fn create(community: &NoIdCommunity) -> Result<Community, String> {
+    get_client().query(
         "INSERT INTO communities ( name, description, public ) VALUES ( $1, $2, $3 ) RETURNING id",
-        &[&community.name, &community.description, &community.public]).unwrap() {
-        new_community = Ok(Community{
-            id: row.get(0),
-            name: community.name.to_string(),
-            description: community.description.to_string(),
-            public: community.public
+        &[&community.name, &community.description, &community.public]
+    )
+        .map_err(|err| err.to_string())
+        .map(|rows|{
+            let row = rows.last().unwrap();
+            Community{
+                id: row.get(0),
+                name: community.name.to_string(),
+                description: community.description.to_string(),
+                public: community.public
+            }
         })
-    }
-    return new_community;
 }
 
-pub fn update(community: &Community) -> Result<Community, &'static str> {
+pub fn update(community: &Community) -> Result<Community, String> {
     get_client().execute(
         "UPDATE communities SET name = $1, description = $2, public = $3 WHERE id = $4",
         &[&community.name, &community.description, &community.public, &community.id]
-    ).unwrap();
-    let new_community = Ok(Community {
-        id: community.id,
-        name: community.name.to_string(),
-        description: community.description.to_string(),
-        public: community.public
-    });
-    return new_community;
+    )
+        .map_err(|err| err.to_string())
+        .map(|_| {
+            Community {
+                id: community.id,
+                name: community.name.to_string(),
+                description: community.description.to_string(),
+                public: community.public
+            }
+        })
 }
